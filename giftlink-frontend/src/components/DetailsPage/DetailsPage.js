a2603f58c31e718785756bc0dbb3afb3e730b657
MarkLewisII <mark.lewis070301@gmail.com>
2025-12-25 02:41:02 -0500

    fixed build issues

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -13,14 +13,11 @@ function DetailsPage() {
 	useEffect(() => {
         const authenticationToken = sessionStorage.getItem('auth-token');
         if (!authenticationToken) {
-			// Task 1: Check for authentication and redirect
              navigate('/app/login');
         }
 
-        // get the gift to be rendered on the details page
         const fetchGift = async () => {
             try {
-				// Task 2: Fetch gift details
                 const response = await fetch(`${urlConfig.backendUrl}/api/gifts/${productId}`);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
@@ -36,18 +33,15 @@ function DetailsPage() {
 
         fetchGift();
 
-		// Task 3: Scroll to top on component mount
 		window.scrollTo(0,0);
 
-    }, [productId]);
+    }, [productId, navigate]);
 
 
     const handleBackClick = () => {
-		// Task 4: Handle back click
 		navigate(-1);
 	};
 
-	//The comments have been hardcoded for this project.
     const comments = [
         {
             author: "John Doe",
@@ -86,7 +80,6 @@ return (
                 <div className="card-body">
                     <div className="image-placeholder-large">
                         {gift.image ? (
-			// Task 5: Display gift image
 			<img src={gift.image} alt={gift.name} className='product-image-large' />
                         ) : (
                             <div className="no-image-available-large">No Image Available</div>

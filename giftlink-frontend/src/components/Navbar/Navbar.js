71beae6e6aa078f6ae062f1889b922f9af04ce66
MarkLewisII <mark.lewis070301@gmail.com>
2025-12-25 02:13:13 -0500

    fixed package.json and other build error

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { urlConfig } from '../../config';
 import { useAppContext } from '../../context/AuthContext';
 
+
 export default function Navbar() {
     const { isLoggedIn, setIsLoggedIn, userName, setUserName } = useAppContext();
     
@@ -57,7 +58,7 @@ export default function Navbar() {
             <ul className="navbar-nav ml-auto">
             {isLoggedIn ? (
                                     <>
-                                    <li className="nav-item"> <span className="nav-link" style={{color: "black", cursor:"pointer"}} onClick={profileSecton}>Welcome, {userName}</span> </li>
+                                    <li className="nav-item"> <span className="nav-link" style={{color: "black", cursor:"pointer"}} onClick={profileSection}>Welcome, {userName}</span> </li>
                                     <li className="nav-item"><button className="nav-link login-btn" onClick={handleLogout}>Logout</button></li>
                                     </>
                                     )  : (
